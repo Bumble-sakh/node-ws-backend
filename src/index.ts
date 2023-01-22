f83e31d022c22458285b1f8f5c28994515ac2308
Bumble <bumble.sakh@gmail.com>
2023-01-23 01:09:32 +1100

    refactor: add streams

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 import dotenv from 'dotenv';
 import { emitter, messageReducer } from './messageReducer';
 import { COMMANDS } from './constants/constants';
@@ -16,53 +16,76 @@ server.on('listening', () => console.log(`Server is listening on port ${PORT}`))
 server.on('connection', (server) => {
   server.on('message', messageReducer);
 
+  const duplex = WebSocket.createWebSocketStream(server, { encoding: 'base64', decodeStrings: false });
+
   emitter.on(COMMANDS.MOUSE_POSITION, async () => {
     const { x, y } = await getPosition();
 
-    server.send(`mouse_position ${x},${y}`);
+    console.log(`Response: mouse_position ${x},${y}`);
+    duplex.write(`mouse_position ${x},${y}`, 'base64');
   });
 
   emitter.on(COMMANDS.MOUSE_UP, async (value) => {
     await moveUp(value);
-    server.send(`mouse_up`);
+
+    console.log(`Response: mouse_up`);
+    duplex.write(`mouse_up`, 'base64');
   });
 
   emitter.on(COMMANDS.MOUSE_DOWN, async (value) => {
     await moveDown(value);
-    server.send(`mouse_down`);
+
+    console.log(`Response: mouse_down`);
+    duplex.write(`mouse_down`, 'base64');
   });
 
   emitter.on(COMMANDS.MOUSE_LEFT, async (value) => {
     await moveLeft(value);
-    server.send(`mouse_left`);
+
+    console.log(`Response: mouse_left`);
+    duplex.write(`mouse_left`, 'base64');
   });
 
   emitter.on(COMMANDS.MOUSE_RIGHT, async (value) => {
     await moveRight(value);
-    server.send(`mouse_right`);
+
+    console.log(`Response: mouse_right`);
+    duplex.write(`mouse_right`, 'base64');
   });
 
   emitter.on(COMMANDS.DRAW_CIRCLE, async (radius) => {
     await circle(radius);
-    server.send(`draw_circle`);
+
+    console.log(`Response: draw_circle`);
+    duplex.write(`draw_circle`, 'base64');
   });
 
   emitter.on(COMMANDS.DRAW_RECTANGLE, async (width, height) => {
     await rectangle(width, height);
-    server.send(`draw_rectangle`);
+
+    console.log(`Response: draw_rectangle`);
+    duplex.write(`draw_rectangle`, 'base64');
   });
 
   emitter.on(COMMANDS.DRAW_SQUARE, async (width) => {
     await square(width);
-    server.send(`draw_square`);
+
+    console.log(`Response: draw_square`);
+    duplex.write(`draw_square`, 'base64');
   });
 
-  emitter.on(COMMANDS.PRNT_SCRN, async (width) => {
-    await printScreen();
-    server.send(`prnt_scrn`);
+  emitter.on(COMMANDS.PRNT_SCRN, async () => {
+    const data = await printScreen();
+
+    console.log(`Response: prnt_scrn ${data}`);
+    duplex.write(`prnt_scrn ${data}`, 'base64');
   });
 
   server.send(`Connected...`);
+
+  duplex.on('close', function () {
+    console.log('The duplex channel has closed');
+  });
 });
 
 server.on('error', (error) => console.error(error));
