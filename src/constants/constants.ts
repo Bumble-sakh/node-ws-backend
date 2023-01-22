66decba977a3f39dc8bae3b591da1f147afee459
Bumble <bumble.sakh@gmail.com>
2023-01-22 20:50:28 +1100

    feat: add commands constant

---
+++ src/constants/constants.ts
@@ -0,0 +1,11 @@
+export enum COMMANDS {
+  'MOUSE_POSITION' = 'mouse_position',
+  'MOUSE_UP' = 'mouse_up',
+  'MOUSE_DOWN' = 'mouse_down',
+  'MOUSE_LEFT' = 'mouse_left',
+  'MOUSE_RIGHT' = 'mouse_right',
+  'DRAW_CIRCLE' = 'draw_circle',
+  'DRAW_RECTANGLE' = 'draw_rectangle',
+  'DRAW_SQUARE' = 'draw_square',
+  'PRNT_SCRN' = 'prnt_scrn',
+}
